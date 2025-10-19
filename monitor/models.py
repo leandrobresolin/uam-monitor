6df0d40cd76c4faf8c7eb88259a865a471dcd3cc
Leandro Bresolin <leandrobresolin@gmail.com>
2025-10-19 14:13:17 -0300

    Creating models

--- monitor/models.py
+++ monitor/models.py
@@ -1,3 +1,55 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class AircraftType(models.Model):
+
+    class EnergyType(models.Model):
+        ELECTRIC = "ELECTRIC"
+        FUEL = "FUEL"
+
+    class ModelType(models.Model):
+        EVTOL = "EVTOL"
+        DRONE = "DRONE"
+        HELICOPTER = "HELICOPTER"
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    manufacturer = models.CharField(max_length=100)
+    energy_type = models.CharField(
+        max_length=10, choices=EnergyType, default=EnergyType.ELECTRIC
+    )
+    model_type = models.CharField(
+        max_length=10, choices=ModelType, default=ModelType.EVTOL
+    )
+
+    def __str__(self):
+        return f"{self.manufacturer} - {self.name} - {self.model_type}"
+
+
+class Aircraft(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    tail_number = models.CharField(max_length=20, unique=True)
+    model = models.ForeignKey(
+        AircraftType, on_delete=models.CASCADE, related_name="aircrafts"
+    )
+    year = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.registration
+
+
+class AircraftData(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    aircraft = models.ForeignKey(
+        Aircraft, on_delete=models.CASCADE, related_name="data_records"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    altitude = models.FloatField()
+    speed = models.FloatField()
+    fuel_level = models.FloatField()
+
+    def __str__(self):
+        return f"Data for {self.aircraft.registration} at {self.date_recorded}"
