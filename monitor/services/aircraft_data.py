18e6dfb87573352b4e27a0213313980faf93dfff
Leandro Bresolin <leandrobresolin@gmail.com>
2026-02-21 08:26:36 -0300

    Add new filter

--- monitor/services/aircraft_data.py
+++ monitor/services/aircraft_data.py
@@ -24,6 +24,8 @@ class AircraftDataService:
             queryset = queryset.filter(id=filters.id)
         if filters.flight_instance is not None:
             queryset = queryset.filter(flight_instance_id=filters.flight_instance)
+        if filters.aircraft is not None:
+            queryset = queryset.filter(flight_instance__aircraft=filters.aircraft)
         if filters.created_at is not None:
             queryset = queryset.filter(created_at__gte=filters.created_at)
         if filters.updated_at is not None:
